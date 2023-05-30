8348fe7c80be30c80ee2c26c82cf14d07dd5f1ad
govinda-attal <govinda.attal@gmail.com>
2023-05-30 16:37:20 -0400

    incuded: json schema & binary app source to generate same

---
+++ src/bin/schema.rs
@@ -0,0 +1,10 @@
+use bidding_platform::msg::{InstantiateMsg, ExecuteMsg, QueryMsg};
+use cosmwasm_schema::write_api;
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
